cc1fe25644f3784da811145d3c6ec957f3857c62
Marina Pereira Alves <65171490+AlvesMarina@users.noreply.github.com>
2020-10-13 20:59:13 -0300

    13
    Exercício 13 OO

---
+++ 13/Condição.java
@@ -0,0 +1,10 @@
+public class Condicao {
+ public static void main(String[] args){
+	int x=2;
+	int y=3;
+	if(x>y){
+		System.out.println("x maior que y")
+	}else
+		System.out.println("x menor que y")
+}
+}
---
+++ 13/numMaior.java
@@ -0,0 +1,7 @@
+public class numMaior{
+ int x=10;
+ int y=20;
+  public static void main(String[] args){
+	System.out.println(Math.max(x,y));
+}
+}
